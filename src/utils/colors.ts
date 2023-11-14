f7d58143904255ee862d27f09371cb92803dde63
Albert Caloracan <albert.caloracan@gmail.com>
2023-11-14 14:33:05 +0800

    feat(ACCOUNTS): create color constant

---
+++ src/utils/colors.ts
@@ -0,0 +1,13 @@
+export const COLORS = {
+  primaryGreen: '#00D373',
+  darkGreen: '#183D33',
+
+  chineseBlack: '#111111',
+  brightGrey: '#EEEEEE',
+  culturedGrey: '#F6F6F6',
+  coolGrey: '#8C8CA1',
+  white: '#FFFFFF',
+  black: '#000000',
+
+  statusGreen: '#00CC00',
+};
