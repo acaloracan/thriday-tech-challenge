4666926b2bbf92f68b374edb5995b7e0833d7870
Albert Caloracan <albert.caloracan@gmail.com>
2023-11-14 14:44:14 +0800

    feat(ALL): create navigation constants

---
+++ src/navigation/constants/navigation.ts
@@ -0,0 +1,6 @@
+export const HOME_NAV = 'Home';
+export const ACCOUNTS_NAV = 'Accounts';
+export const PAYMENTS_NAV = 'Payments';
+export const INVOICES_NAV = 'Invoices';
+export const TAX_NAV = 'Tax';
+export const SETTINGS_NAV = 'Settings';
